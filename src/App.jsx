498b97606c66106915da11940178ee75210c3d39
ladd lindsay <laddrlindsay@gmail.com>
2023-03-28 19:52:52 -0700

    finished the exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,89 @@ import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+  
     this.state = {
       list: ["ready", "set", "GO"],
       text: "",
     };
-      this.onSubmit = this.onSubmit.bind(this)
+
+    this.onSubmit = this.onSubmit.bind(this);
   }
-onSubmit(event){
-  event.preventDefault()
-  let newList = [...this.state.list, this.state.list]
-  this.setState({
-    list: newList,
-    text: ""
-  })
-}
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    let newList = [...this.state.list, this.state.text];
+    this.setState({ list: newList, text: "" })
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello World!</h1>
         <form onSubmit={this.onSubmit}>
-          <input type="text" name="text" id="text" value = {this.state.text} onChange = {(event) => this.setState ({
-            text: event.target.value,
-          })}>
-            <button type='submit'>Submit</button>
-          </input>
+        <input 
+        type="text" 
+        name="text" 
+        id="text" 
+        value={this.state.text} 
+        onChange={(event) => this.setState({ text: event.target.value})} 
+        />
+        <button type="submit">Add</button>
         </form>
         <ul>
-          {this.state.list.map((element, index) => {
-            return <li key = {index}>
-              {element}
-            </li>
+          {this.state.list.map((item, index) => {
+            return <li key={item + index}>{item}</li>;
           })}
         </ul>
       </div>
-    );
+    )
   }
 }
+
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+    
+//     this.state = {
+//       list: ["ready", "set", "GO"],
+//       text: "",
+//     };
+//       this.onSubmit = this.onSubmit.bind(this);
+//   }
+// onSubmit(event){
+//   event.preventDefault()
+//   let newList = [...this.state.list, this.state.list]
+//   this.setState({
+//     list: newList,
+//     text: "",
+//   })
+// }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World</h1>
+//         <form onSubmit={this.onSubmit}>
+//           <input type="text" name="text" id="text" value = {this.state.text} onChange = {(event) => this.setState ({
+//             text: event.target.value
+//           })}>
+//             <button type="submit">Submit</button>
+//           </input>
+//         </form>
+//         <ul>
+//           {this.state.list.map((element, index) => {
+//             return <li key = {element + index}>
+//               {element}
+//             </li>
+//           })}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
